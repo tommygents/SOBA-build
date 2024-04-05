a575c1e1c490c50c399f7c352c8fc43a03c72f4f
tommygents <tmaranges+gh@gmail.com>
2024-04-05 16:45:14 -0400

    Mapped out Wave Manager, added todos and functions to start and end timer

--- Assets/Scripts/Utility Scripts/WaveManager.cs
+++ Assets/Scripts/Utility Scripts/WaveManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,13 @@ public class WaveManager : MonoBehaviour
     public float waveDuration = 60f;
     public float breakDuration = 120f;
 
+    public bool isWaveActive = false;
+    public float timer = 0f;
+
     //TODO: A timer UI, UI that shows what wave number we're on
+
+    public static event Action OnWaveEnd;
+    public static event Action<int> OnWaveStart; //passes the number of the wave to delegates
 
 
     // Start is called before the first frame update
@@ -22,8 +29,23 @@ public class WaveManager : MonoBehaviour
     void Update()
     {
         
+        
+        if (timer <= 0f)
+        {
+            //TODO: Logic that switches out the timers and either starts and ends a wave.
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+            
+        }
+
+       //TODO: Update the GUI
+
+
     }
 
+    
 
     /*
      * The Wave Manager runs the timer and the count of which wave we're on. It gives information to a UI,
@@ -31,6 +53,40 @@ public class WaveManager : MonoBehaviour
      * 
      * TODO: Set two timers: one for the break, and one for the wave itself.
      * TODO: A switch that tells us whether we're in wave or in break
-     * TODO: A UI that 
+     * TODO: A UI that lets the player track the timer
+     * TODO: The wave manager lets subscribers know that a wave has started or ended, and maybe also which wave?
      */
+
+
+    public void EndWave()
+    {
+        isWaveActive = false;
+        OnWaveEnd?.Invoke();
+        //TODO: Let the UI know to switch over to a break session
+        //TODO: Iterate the wave number so that it's an upcoming wave and the UI knows/indicates that it's an upcoming wave
+    }
+
+    public void StartWave()
+    {
+        isWaveActive = true;
+        OnWaveStart?.Invoke(waveNumber);
+        //TODO: Let the UI know to start a wave
+        //TODO: create a w
+    }
+
+    public void SwitchTimers()
+    {
+        if (isWaveActive)
+        {
+            //TODO: Switch to wave-off timer
+            EndWave();
+
+        }
+        else
+        {
+            //TODO: switch to wave-on timer
+            StartWave();
+
+        }
+    }
 }
