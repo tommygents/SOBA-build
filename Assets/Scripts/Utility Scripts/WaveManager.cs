f71aab9e6675c3f0089220d109391bb2576d14f1
tommygents <tmaranges+gh@gmail.com>
2024-04-05 12:44:39 -0400

    Began work on wave-manager

---
+++ Assets/Scripts/Utility Scripts/WaveManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+
+    public int waveNumber = 0;
+    public float waveDuration = 60f;
+    public float breakDuration = 120f;
+
+    //TODO: A timer UI, UI that shows what wave number we're on
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
