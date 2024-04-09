a17e6cfad2b27c86de41ff5f892ba91f9924881c
tommygents <tmaranges+gh@gmail.com>
2024-04-09 14:04:27 -0400

    Added UI for wave manager that tracks timer and wave number

--- Assets/Scripts/Utility Scripts/WaveManager.cs
+++ Assets/Scripts/Utility Scripts/WaveManager.cs
@@ -6,14 +6,15 @@ using UnityEngine;
 public class WaveManager : MonoBehaviour
 {
 
-    public int waveNumber = 0;
+    public int waveNumber = 1;
     public float waveDuration = 60f;
     public float breakDuration = 120f;
 
     public bool isWaveActive = false;
     public float timer = 0f;
 
-    //TODO: A timer UI, UI that shows what wave number we're on
+    public WaveManagerUI waveUI;
+    
 
     public static event Action OnWaveEnd;
     public static event Action<int> OnWaveStart; //passes the number of the wave to delegates
@@ -22,7 +23,7 @@ public class WaveManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
@@ -32,7 +33,18 @@ public class WaveManager : MonoBehaviour
         
         if (timer <= 0f)
         {
-            //TODO: Logic that switches out the timers and either starts and ends a wave.
+            if (isWaveActive)
+            {
+                
+                EndWave();
+
+            }
+            else
+            {
+                
+                StartWave();
+
+            }
         }
         else
         {
@@ -40,7 +52,8 @@ public class WaveManager : MonoBehaviour
             
         }
 
-       //TODO: Update the GUI
+        //TODO: Update the GUI
+        waveUI.UpdateTimer(timer);
 
 
     }
@@ -51,10 +64,10 @@ public class WaveManager : MonoBehaviour
      * The Wave Manager runs the timer and the count of which wave we're on. It gives information to a UI,
      * and other components look to it to see where we're at. (Or perhaps they listen for an event.)
      * 
-     * TODO: Set two timers: one for the break, and one for the wave itself.
-     * TODO: A switch that tells us whether we're in wave or in break
+     * Set two timers: one for the break, and one for the wave itself.
+     * A switch that tells us whether we're in wave or in break
      * TODO: A UI that lets the player track the timer
-     * TODO: The wave manager lets subscribers know that a wave has started or ended, and maybe also which wave?
+     * The wave manager lets subscribers know that a wave has started or ended, and also which wave
      */
 
 
@@ -62,31 +75,25 @@ public class WaveManager : MonoBehaviour
     {
         isWaveActive = false;
         OnWaveEnd?.Invoke();
+        waveNumber++;
         //TODO: Let the UI know to switch over to a break session
-        //TODO: Iterate the wave number so that it's an upcoming wave and the UI knows/indicates that it's an upcoming wave
+        //TODO: Make UI update to reflect that it's an upcoming wave
+        timer = breakDuration; //Start the break timer
+        waveUI.isWaveActiveUI(isWaveActive);
+        waveUI.waveNumberUI(waveNumber);
     }
 
     public void StartWave()
     {
         isWaveActive = true;
         OnWaveStart?.Invoke(waveNumber);
+        timer = waveDuration; //Start the wave timer
         //TODO: Let the UI know to start a wave
-        //TODO: create a w
+        waveUI.isWaveActiveUI(isWaveActive);
+        waveUI.waveNumberUI(waveNumber);
+
+
     }
 
-    public void SwitchTimers()
-    {
-        if (isWaveActive)
-        {
-            //TODO: Switch to wave-off timer
-            EndWave();
-
-        }
-        else
-        {
-            //TODO: switch to wave-on timer
-            StartWave();
-
-        }
-    }
+    
 }
