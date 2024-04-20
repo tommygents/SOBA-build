d9ed83fc1e4cd757f75b03a6d90b49166b39057d
tommygents <tmaranges+gh@gmail.com>
2024-04-20 13:44:32 -0400

    Began work on the tutorial

--- Assets/Scripts/Utility Scripts/GameEvents.cs
+++ Assets/Scripts/Utility Scripts/GameEvents.cs
@@ -5,6 +5,7 @@ public static class GameEvents
     // Define an event that takes an int parameter for the score
     public static event Action<int> OnEnemyKilled;
 
+
     // Method to trigger the event
     public static void EnemyKilled(int score)
     {
